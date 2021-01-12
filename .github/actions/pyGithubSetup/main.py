e06c7dc5feb7c913cff9eb0a3d1cab331fa7c1c5
sachinsharmaepisource <77056318+sachinsharmaepisource@users.noreply.github.com>
2021-01-12 15:18:25 +0530

    Update main.py

--- .github/actions/pyGithubSetup/main.py
+++ .github/actions/pyGithubSetup/main.py
@@ -6,18 +6,18 @@ from datetime import datetime
 def get_datetime_now():
   now = datetime.now().replace(microsecond=0)
   return now
+
 def get_release_message(repo):
   releaseMessage = ''
   startDate = get_datetime_now()
-  endDate = get_datetime_now()
   if repo.get_releases().totalCount == 0:
     startDate = repo.created_at
   else:
     latestRelease = repo.get_latest_release()
     startDate = latestRelease.created_at
-  pulls = get_pull_requests(repo, startDate, endDate, 100)
+  pulls = get_pull_requests(repo, startDate, 100)
   for pull in pulls:
-    releaseMessage = releaseMessage + pull.title + '\n'
+    releaseMessage = releaseMessage + '\n\u2022' + pull.title + '(' + pull.url + ')'
   return releaseMessage
   
 def get_inputs(input_name):
@@ -38,7 +38,7 @@ def get_commits(repo):
   commits = repo.get_commits()
   for commit in commits:
     print(commit)
-def get_pull_requests(repo, start_date, end_date, max_pull_requests):
+def get_pull_requests(repo, start_date, max_pull_requests):
   print('Began get_pull_requests()')
   print('Total Count of pull requests that have been closed = ', repo.get_pulls(state='closed').totalCount)
   pulls: List[PullRequest.PullRequest] = []
@@ -49,19 +49,12 @@ def get_pull_requests(repo, start_date, end_date, max_pull_requests):
         continue
       merged_dt = pull.merged_at
       updated_dt = pull.updated_at
-      print('merged_dt', pull.merged_at, 'updated_dt', pull.updated_at)
-      print('start_date',start_date,'end_date',end_date)
+      print('merged_dt ', merged_dt, '\nupdated_dt ', updated_dt)
+      print('start_date',start_date)
       if merged_dt >= start_date:
         print(pull.title)
         pulls_str += pull.title
         pulls.append(pull)
-#       if merged_dt > end_date:
-#         continue
-#       if updated_dt < start_date:
-#         return pulls
-      
-#       if len(pulls) >= max_pull_requests:
-#         return pulls
   except RequestException as e:
       print('Github pulls error (request)', e)
   except GithubException as e:
@@ -87,8 +80,6 @@ def main():
   isPrerelease = False
   print('releaseMessage:', releaseMessage)
   
-  
-  
   if repo.get_releases().totalCount == 0:
     print('There is no previous release, so lastVerseion is set to default - v0.0.0')
     lastVersion = 'v0.0.0'
@@ -99,7 +90,7 @@ def main():
 
 
   if lastVersion < currentVersion:
-    releaseName = releaseName + currentVersion + '' + REPO_NAME
+    releaseName = releaseName + ' ' + currentVersion + ' of ' + REPO_NAME
     release = createRelease(repo, currentVersion, tagMessage, releaseName, releaseMessage, isDraft, isPrerelease)
     print('Creation of new Release is completed with its tag name as', release.tag_name)
   elif lastVersion == currentVersion:
