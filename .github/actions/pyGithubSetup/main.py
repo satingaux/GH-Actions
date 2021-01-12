421138f433e0d95e54e5c32e5a9c97f8eda66657
sachinsharmaepisource <77056318+sachinsharmaepisource@users.noreply.github.com>
2021-01-12 13:25:31 +0530

    Update main.py

--- .github/actions/pyGithubSetup/main.py
+++ .github/actions/pyGithubSetup/main.py
@@ -29,8 +29,9 @@ def get_pull_requests(repo, start_date, end_date, max_pull_requests):
     for pull in repo.get_pulls(state='closed', sort='updated', direction='desc'):
       if not pull.merged_at:
         continue
-#       merged_dt = dtutil.UTC_TZ.localize(pull.merged_at)
-#       updated_dt = dtutil.UTC_TZ.localize(pull.updated_at)
+      merged_dt = dtutil.UTC_TZ.localize(pull.merged_at)
+      updated_dt = dtutil.UTC_TZ.localize(pull.updated_at)
+      print('merged_dt', merged_dt, 'updated_dt', updated_dt)
 #       if merged_dt > end_date:
 #         continue
 #       if updated_dt < start_date:
@@ -63,6 +64,7 @@ def main():
   releaseMessage = 'Release Message as the list of all the PRs title ' + get_pull_requests(repo, 0, 0, 0)
   isDraft = False
   isPrerelease = False
+  latestReleaseDate = ''
   
   
   
